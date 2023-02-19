8f6532d3d912cfeb77afbd555f773003c40f4760
Coderboy80 <65282069+Coderboy80@users.noreply.github.com>
2023-02-20 00:26:25 +0530

    add: Create PL w/ songs from all ur PLs

--- bundle.js
+++ bundle.js
@@ -1,11 +1,12 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const accessToken = new URLSearchParams(window.location.hash.substring(1)).get(
+let accessToken = new URLSearchParams(window.location.hash.substring(1)).get(
   "access_token"
 );
 
 const topTracks = document.querySelector("#top-tracks");
 const topArtists = document.querySelector("#top-artists");
 const btnTop50 = document.querySelector("#btn-top-50");
+const btnMixUp = document.querySelector("#btn-mix-up");
 
 var SpotifyWebApi = require("spotify-web-api-js");
 
@@ -16,45 +17,12 @@ let userId;
 spotifyApi.getMe().then(
   function (data) {
     userId = data.id;
-    const options = { limit: 10 };
-    spotifyApi.getMyTopTracks(options).then(
-      function (data) {
-        let tracks = ("User information", data.items);
-        tracks.forEach((element) => {
-          let previewUrl = element.preview_url;
-          const audio = document.createElement("audio");
-          audio.controls = true;
-          audio.src = previewUrl;
-          const trackName = document.createElement("h3");
-          trackName.textContent = element.name;
-          topTracks.insertAdjacentElement("beforeend", trackName);
-          topTracks.insertAdjacentElement("beforeend", audio);
-        });
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
 
-    spotifyApi.getMyTopArtists(options).then(
-      function (data) {
-        const artists = data.items;
-        artists.forEach((artist) => {
-          const artistPfpSource = artist.images[2].url;
-          const artistPfp = document.createElement("img");
-          artistPfp.src = artistPfpSource;
-          const artistName = document.createElement("h3");
-          artistName.textContent = artist.name;
-          topArtists.insertAdjacentElement("beforeend", artistName);
-          topArtists.insertAdjacentElement("beforeend", artistPfp);
-        });
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
     btnTop50.addEventListener("click", function () {
-      createNewPlaylist();
+      createNewTop50TrackPlaylist();
+    });
+    btnMixUp.addEventListener("click", function () {
+      createMixUpPlaylist();
     });
   },
   function (err) {
@@ -62,11 +30,120 @@ spotifyApi.getMe().then(
   }
 );
 
-function createNewPlaylist() {
+const options = { limit: 10 };
+spotifyApi.getMyTopTracks(options).then(
+  function (data) {
+    let tracks = ("User information", data.items);
+    tracks.forEach((track) => {
+      let previewUrl = track.preview_url;
+      const audio = document.createElement("audio");
+      audio.controls = true;
+      audio.src = previewUrl;
+      const trackName = document.createElement("h3");
+      if (track.artists.length === 1)
+        trackName.textContent = `${track.name} by ${track.artists[0].name}`;
+      else {
+        trackName.textContent = `${track.name} by ${track.artists
+          .map((ele) => ele.name)
+          .join(", ")}`;
+      }
+      topTracks.insertAdjacentElement("beforeend", trackName);
+      topTracks.insertAdjacentElement("beforeend", audio);
+    });
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
+spotifyApi.getMyTopArtists(options).then(
+  function (data) {
+    const artists = data.items;
+    artists.forEach((artist) => {
+      const artistPfpSource = artist.images[2].url;
+      const artistPfp = document.createElement("img");
+      artistPfp.src = artistPfpSource;
+      const artistName = document.createElement("h3");
+      artistName.textContent = artist.name;
+      topArtists.insertAdjacentElement("beforeend", artistName);
+      topArtists.insertAdjacentElement("beforeend", artistPfp);
+    });
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
+function createMixUpPlaylist() {
+  const playlistName = `Mix Up Tracks - ${Intl.DateTimeFormat(
+    navigator.locale
+  ).format(Date.now())} `;
+  const playlistDescription =
+    "Random songs from all your playlists - Created By SpotOn";
+  spotifyApi
+    .createPlaylist(userId, {
+      name: playlistName,
+      description: playlistDescription,
+      public: false,
+    })
+    .then((response) => {
+      const playlistId = response.id;
+      addMixUpTracks(playlistId);
+    })
+    .catch((error) => {
+      console.error(`Error creating new playlist: ${error}`);
+    });
+}
+
+async function addMixUpTracks(plId) {
+  const mixTracks = [];
+
+  try {
+    const data = await spotifyApi.getUserPlaylists(userId, { limit: 50 });
+    for (const item of data.items) {
+      const i = item.id;
+      const trackData = await spotifyApi.getPlaylistTracks(i, { limit: 50 });
+      const randomTrack =
+        trackData.items[Math.floor(Math.random() * trackData.items.length)]
+          ?.track.uri;
+      if (randomTrack) {
+        mixTracks.push(randomTrack);
+      }
+    }
+    await spotifyApi.addTracksToPlaylist(plId, mixTracks);
+  } catch (error) {
+    console.error(`Error adding new tracks: ${error}`);
+  }
+}
+
+// function addMixUpTracks(plId) {
+//   const mixTracks = [];
+//   console.log(typeof mixTracks);
+//   spotifyApi
+//     .getUserPlaylists(userId, { limit: 50 })
+//     .then(function (data) {
+//       data.items.forEach((item) => {
+//         const i = item.id;
+//         spotifyApi.getPlaylistTracks(i, { limit: 50 }).then(function (data) {
+//           const randomTrack =
+//             data.items[Math.floor(Math.random() * data.items.length)]?.track
+//               .uri;
+//           randomTrack && mixTracks.push(randomTrack);
+//         });
+//       });
+
+//     })
+//     .catch((error) => {
+//       console.error(`Error adding new tracks: ${error}`);
+//     });
+// }
+
+function createNewTop50TrackPlaylist() {
   const playlistName = `Top 50 tracks - ${Intl.DateTimeFormat(
     navigator.locale
   ).format(Date.now())} `;
-  const playlistDescription = "Your current top 50 tracks, all in one playlist";
+  const playlistDescription =
+    "Your current top 50 tracks, all in one playlist - Created By SpotOn";
   spotifyApi
     .createPlaylist(userId, {
       name: playlistName,
@@ -96,44 +173,6 @@ function addTop50Tracks(plId) {
     }
   );
 }
-
-// const options = { limit: 10 };
-// spotifyApi.getMyTopTracks(options).then(
-//   function (data) {
-//     let tracks = ("User information", data.items);
-//     tracks.forEach((element) => {
-//       let previewUrl = element.preview_url;
-//       const audio = document.createElement("audio");
-//       audio.controls = true;
-//       audio.src = previewUrl;
-//       const trackName = document.createElement("h3");
-//       trackName.textContent = element.name;
-//       topTracks.insertAdjacentElement("beforeend", trackName);
-//       topTracks.insertAdjacentElement("beforeend", audio);
-//     });
-//   },
-//   function (err) {
-//     console.error(err);
-//   }
-// );
-
-// spotifyApi.getMyTopArtists(options).then(
-//   function (data) {
-//     const artists = data.items;
-//     artists.forEach((artist) => {
-//       const artistPfpSource = artist.images[2].url;
-//       const artistPfp = document.createElement("img");
-//       artistPfp.src = artistPfpSource;
-//       const artistName = document.createElement("h3");
-//       artistName.textContent = artist.name;
-//       topArtists.insertAdjacentElement("beforeend", artistName);
-//       topArtists.insertAdjacentElement("beforeend", artistPfp);
-//     });
-//   },
-//   function (err) {
-//     console.error(err);
-//   }
-// );
 
 },{"spotify-web-api-js":2}],2:[function(require,module,exports){
 /* global module */
--- ok.js
+++ ok.js
@@ -1,10 +1,11 @@
-const accessToken = new URLSearchParams(window.location.hash.substring(1)).get(
+let accessToken = new URLSearchParams(window.location.hash.substring(1)).get(
   "access_token"
 );
 
 const topTracks = document.querySelector("#top-tracks");
 const topArtists = document.querySelector("#top-artists");
 const btnTop50 = document.querySelector("#btn-top-50");
+const btnMixUp = document.querySelector("#btn-mix-up");
 
 var SpotifyWebApi = require("spotify-web-api-js");
 
@@ -15,45 +16,12 @@ let userId;
 spotifyApi.getMe().then(
   function (data) {
     userId = data.id;
-    const options = { limit: 10 };
-    spotifyApi.getMyTopTracks(options).then(
-      function (data) {
-        let tracks = ("User information", data.items);
-        tracks.forEach((element) => {
-          let previewUrl = element.preview_url;
-          const audio = document.createElement("audio");
-          audio.controls = true;
-          audio.src = previewUrl;
-          const trackName = document.createElement("h3");
-          trackName.textContent = element.name;
-          topTracks.insertAdjacentElement("beforeend", trackName);
-          topTracks.insertAdjacentElement("beforeend", audio);
-        });
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
 
-    spotifyApi.getMyTopArtists(options).then(
-      function (data) {
-        const artists = data.items;
-        artists.forEach((artist) => {
-          const artistPfpSource = artist.images[2].url;
-          const artistPfp = document.createElement("img");
-          artistPfp.src = artistPfpSource;
-          const artistName = document.createElement("h3");
-          artistName.textContent = artist.name;
-          topArtists.insertAdjacentElement("beforeend", artistName);
-          topArtists.insertAdjacentElement("beforeend", artistPfp);
-        });
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
     btnTop50.addEventListener("click", function () {
-      createNewPlaylist();
+      createNewTop50TrackPlaylist();
+    });
+    btnMixUp.addEventListener("click", function () {
+      createMixUpPlaylist();
     });
   },
   function (err) {
@@ -61,11 +29,98 @@ spotifyApi.getMe().then(
   }
 );
 
-function createNewPlaylist() {
+const options = { limit: 10 };
+spotifyApi.getMyTopTracks(options).then(
+  function (data) {
+    let tracks = ("User information", data.items);
+    tracks.forEach((track) => {
+      let previewUrl = track.preview_url;
+      const audio = document.createElement("audio");
+      audio.controls = true;
+      audio.src = previewUrl;
+      const trackName = document.createElement("h3");
+      if (track.artists.length === 1)
+        trackName.textContent = `${track.name} by ${track.artists[0].name}`;
+      else {
+        trackName.textContent = `${track.name} by ${track.artists
+          .map((ele) => ele.name)
+          .join(", ")}`;
+      }
+      topTracks.insertAdjacentElement("beforeend", trackName);
+      topTracks.insertAdjacentElement("beforeend", audio);
+    });
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
+spotifyApi.getMyTopArtists(options).then(
+  function (data) {
+    const artists = data.items;
+    artists.forEach((artist) => {
+      const artistPfpSource = artist.images[2].url;
+      const artistPfp = document.createElement("img");
+      artistPfp.src = artistPfpSource;
+      const artistName = document.createElement("h3");
+      artistName.textContent = artist.name;
+      topArtists.insertAdjacentElement("beforeend", artistName);
+      topArtists.insertAdjacentElement("beforeend", artistPfp);
+    });
+  },
+  function (err) {
+    console.error(err);
+  }
+);
+
+function createMixUpPlaylist() {
+  const playlistName = `Mix Up Tracks - ${Intl.DateTimeFormat(
+    navigator.locale
+  ).format(Date.now())} `;
+  const playlistDescription =
+    "Mixed Up songs from all your playlists - Created By SpotOn";
+  spotifyApi
+    .createPlaylist(userId, {
+      name: playlistName,
+      description: playlistDescription,
+      public: false,
+    })
+    .then((response) => {
+      const playlistId = response.id;
+      addMixUpTracks(playlistId);
+    })
+    .catch((error) => {
+      console.error(`Error creating new playlist: ${error}`);
+    });
+}
+
+async function addMixUpTracks(plId) {
+  const mixTracks = [];
+
+  try {
+    const data = await spotifyApi.getUserPlaylists(userId, { limit: 50 });
+    for (const item of data.items) {
+      const i = item.id;
+      const trackData = await spotifyApi.getPlaylistTracks(i, { limit: 50 });
+      const randomTrack =
+        trackData.items[Math.floor(Math.random() * trackData.items.length)]
+          ?.track.uri;
+      if (randomTrack) {
+        mixTracks.push(randomTrack);
+      }
+    }
+    await spotifyApi.addTracksToPlaylist(plId, mixTracks);
+  } catch (error) {
+    console.error(`Error adding new tracks: ${error}`);
+  }
+}
+
+function createNewTop50TrackPlaylist() {
   const playlistName = `Top 50 tracks - ${Intl.DateTimeFormat(
     navigator.locale
   ).format(Date.now())} `;
-  const playlistDescription = "Your current top 50 tracks, all in one playlist";
+  const playlistDescription =
+    "Your current top 50 tracks, all in one playlist - Created By SpotOn";
   spotifyApi
     .createPlaylist(userId, {
       name: playlistName,
@@ -95,41 +150,3 @@ function addTop50Tracks(plId) {
     }
   );
 }
-
-// const options = { limit: 10 };
-// spotifyApi.getMyTopTracks(options).then(
-//   function (data) {
-//     let tracks = ("User information", data.items);
-//     tracks.forEach((element) => {
-//       let previewUrl = element.preview_url;
-//       const audio = document.createElement("audio");
-//       audio.controls = true;
-//       audio.src = previewUrl;
-//       const trackName = document.createElement("h3");
-//       trackName.textContent = element.name;
-//       topTracks.insertAdjacentElement("beforeend", trackName);
-//       topTracks.insertAdjacentElement("beforeend", audio);
-//     });
-//   },
-//   function (err) {
-//     console.error(err);
-//   }
-// );
-
-// spotifyApi.getMyTopArtists(options).then(
-//   function (data) {
-//     const artists = data.items;
-//     artists.forEach((artist) => {
-//       const artistPfpSource = artist.images[2].url;
-//       const artistPfp = document.createElement("img");
-//       artistPfp.src = artistPfpSource;
-//       const artistName = document.createElement("h3");
-//       artistName.textContent = artist.name;
-//       topArtists.insertAdjacentElement("beforeend", artistName);
-//       topArtists.insertAdjacentElement("beforeend", artistPfp);
-//     });
-//   },
-//   function (err) {
-//     console.error(err);
-//   }
-// );
